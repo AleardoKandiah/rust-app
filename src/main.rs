556d9ca83287fdfef04626035cb4a74c87378795
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-06 12:42:34 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{routes, Build};
+
+#[macro_use] extern create rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "world"
+}
+
+#[launch]
+
+fn rocket() -> _ {
+    rocket::build(): Rocket<Build>
+        .mount(base: "/hello", routes: routes![world])
 }
